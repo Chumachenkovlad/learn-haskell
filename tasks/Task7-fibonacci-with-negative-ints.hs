7770d5d23e7bf619fa4e6f8fbd40e28c350c9d18
Chumachenko Vladyslav <chumachenko.vla@gmail.com>
2019-12-23 00:10:48 +0200

    improve fibonacii algorithm

--- tasks/Task7-fibonacci-with-negative-ints.hs
+++ tasks/Task7-fibonacci-with-negative-ints.hs
@@ -3,5 +3,8 @@ module Task7 where
 fibonacci :: Integer -> Integer
 fibonacci n | n == 0 = 0
             | n == 1 = 1
-            | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
+            | n > 0 = helper 0 1 n 3
             | n < 0 = (-1)^((-1) * n + 1) * fibonacci ((-1) * n)
+
+helper x y n count | count > n = x + y
+                   | count <= n = helper (y) (x + y) n (count + 1)       
